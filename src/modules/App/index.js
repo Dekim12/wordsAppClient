174e024ecd228ff7d5d2b26e74afdee4f61614c9
Siarhei Zykau <zykau_s@mail.ru>
2020-12-15 01:16:49 +0300

    init: init project

---
+++ src/modules/App/index.js
@@ -0,0 +1,13 @@
+import './index.css';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <p>English Words</p>
+      </header>
+    </div>
+  );
+};
+
+export default App;
